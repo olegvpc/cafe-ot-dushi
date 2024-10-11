630593b477e49cd08c8435168c6ef14349ad2cfc
o.tkach <olegvpc@yandex.ru>
2024-10-11 19:59:47 +0700

    - Baht in front

--- resources/views/components/menu/list.blade.php
+++ resources/views/components/menu/list.blade.php
@@ -10,7 +10,7 @@
             <tr>
                 <td style="width: 50px">{{ $menu->id }}</td>
                 <td style="width: 800px">{!! $menu->title !!}</td>
-                <td style="width: 100px">{{ $menu->price }}</td>
+                <td style="width: 100px">{{ $menu->price }} &#3647;</td>
                 <td style="width: 70px">{{ $menu->active ? "Active" : '-'}}</td>
             </tr>
         @endforeach
