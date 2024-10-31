4d62f97ff2036488cbd0f3e4b5eceb4db0654e6f
o.tkach <olegvpc@yandex.ru>
2024-10-31 12:36:55 +0700

    - correct composer.phar

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 echo 'Deploing...'
 git pull origin main
 php8.2 artisan down
-php8.2 composer phar install --no-dev --optimize-autoloader
+php8.2 composer.phar install --no-dev --optimize-autoloader
 php8.2 artisan migrate --force
 php8.2 artisan config:cache
 php8.2 artisan event:cache
